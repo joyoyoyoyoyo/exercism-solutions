74478874d6c161fb00c43b6a4492570fe7f0261e
Angel Ortega <cs.angelortega@gmail.com>
2018-06-15 16:15:26 -0700

    Pending all tests

--- scala/bob/src/test/scala/BobTest.scala
+++ scala/bob/src/test/scala/BobTest.scala
@@ -5,58 +5,72 @@ class BobTest extends FunSuite with Matchers {
 
 
   test("stating something") {
+    pending
     Bob.response("Tom-ay-to, tom-aaaah-to.") should be ("Whatever.")
   }
 
   test("shouting") {
+    pending
     Bob.response("WATCH OUT!") should be ("Whoa, chill out!")
   }
 
   test("shouting gibberish") {
+    pending
     Bob.response("FCECDFCAAB") should be ("Whoa, chill out!")
   }
 
   test("asking a question") {
+    pending
     Bob.response("Does this cryogenic chamber make me look fat?") should be ("Sure.")
   }
 
   test("asking a numeric question") {
+    pending
     Bob.response("You are, what, like 15?") should be ("Sure.")
   }
 
   test("asking gibberish") {
+    pending
     Bob.response("fffbbcbeab?") should be ("Sure.")
   }
 
   test("talking forcefully") {
+    pending
     Bob.response("Let's go make out behind the gym!") should be ("Whatever.")
   }
 
   test("using acronyms in regular speech") {
+    pending
     Bob.response("It's OK if you don't want to go to the DMV.") should be ("Whatever.")
   }
 
   test("forceful question") {
+    pending
     Bob.response("WHAT THE HELL WERE YOU THINKING?") should be ("Calm down, I know what I'm doing!")
   }
 
   test("shouting numbers") {
+    pending
     Bob.response("1, 2, 3 GO!") should be ("Whoa, chill out!")
   }
 
   test("only numbers") {
+    pending
     Bob.response("1, 2, 3") should be ("Whatever.")
   }
 
   test("question with only numbers") {
+    pending
     Bob.response("4?") should be ("Sure.")
   }
 
   test("shouting with special characters") {
+    pending
     Bob.response("ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!") should be ("Whoa, chill out!")
   }
 
   test("shouting with no exclamation mark") {
+    pending
     Bob.response("I HATE YOU") should be ("Whoa, chill out!")
   }
 
