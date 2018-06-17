e5152e54efab0f5aabf474d4083ba416417e2a0f
Angel <cs.angelortega@gmail.com>
2018-06-17 16:21:14 -0700

    All tests are passing. Note onVenus contains different behavior for rounding

--- scala/space-age/src/main/scala/SpaceAge.scala
+++ scala/space-age/src/main/scala/SpaceAge.scala
@@ -1,20 +1,20 @@
 import scala.math.BigDecimal.RoundingMode
 
 object SpaceAge {
-  def onEarth(seconds: Double): BigDecimal = BigDecimal(seconds / 31557600.0).setScale(2, RoundingMode.CEILING)
+  def onEarth(seconds: Double): BigDecimal = BigDecimal(seconds / 31557600.0).setScale(2, RoundingMode.HALF_DOWN)
 
-  def onMercury(seconds: Double): BigDecimal = this.onEarth(seconds) / 0.2408467
+  def onMercury(seconds: Double): BigDecimal = (this.onEarth(seconds) / BigDecimal(0.2408467)).setScale(2, RoundingMode.HALF_DOWN)
 
-  def onVenus(seconds: Double): BigDecimal = this.onEarth(seconds) /  0.61519726
+  def onVenus(seconds: Double): BigDecimal = (this.onEarth(seconds) /  BigDecimal(0.61519726)).setScale(2, RoundingMode.DOWN)
 
-  def onMars(seconds: Double): BigDecimal = this.onEarth(seconds) / 1.8808158
+  def onMars(seconds: Double): BigDecimal = (this.onEarth(seconds) / BigDecimal(1.8808158)).setScale(2, RoundingMode.HALF_DOWN)
 
-  def onJupiter(seconds: Double): BigDecimal = this.onEarth(seconds) / 11.862615
+  def onJupiter(seconds: Double): BigDecimal = (this.onEarth(seconds) / BigDecimal(11.862615)).setScale(2, RoundingMode.HALF_DOWN)
 
-  def onSaturn(seconds: Double): BigDecimal = this.onEarth(seconds) / 29.447498
+  def onSaturn(seconds: Double): BigDecimal = (this.onEarth(seconds) / BigDecimal(29.447498)).setScale(2, RoundingMode.HALF_DOWN)
 
-  def onUranus(seconds: Double): BigDecimal = this.onEarth(seconds) / 84.016846
+  def onUranus(seconds: Double): BigDecimal = (this.onEarth(seconds) / BigDecimal(84.016846)).setScale(2, RoundingMode.HALF_DOWN)
 
-  def onNeptune(seconds: Double): BigDecimal = this.onEarth(seconds) / 164.79132
+  def onNeptune(seconds: Double): BigDecimal = (this.onEarth(seconds) / BigDecimal(164.79132)).setScale(2, RoundingMode.HALF_DOWN)
 
 }
